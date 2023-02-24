5be0166042ac71d50c191482d4c9b385f06abc4a
Dan Rees <73440070+danielrees8743@users.noreply.github.com>
2023-02-24 11:43:36 +0000

    Added env.sampe to git, few changes to photoController.js

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -4,30 +4,29 @@ const axios = require('axios');
 const url = process.env.UNSPLASH_API_URL;
 const key = process.env.UNSPLASH_ACCESS_KEY;
 
-// GET /api/photos
-// Get all favorites
-// Public
+//- GET /api/photos
+//- Get all favorites
+//- Public
 exports.getAllPhotos = async (req, res) => {
   try {
     const { data } = await axios.get(`${url}/photos?client_id=${key}`);
-    // const photos = data.map((photo) => photo.urls.raw);
+
     const photos = data.map((photo) => {
       return {
         photo: photo.urls.raw,
       };
     });
     res.status(200).json({ photos });
-    // res.status(200).json({ data });
   } catch (error) {
-    res.status(400).json({
+    res.status(500).json({
       message: 'Server error. Please try again later.',
     });
   }
 };
 
-// GET /api/photos/:id
-// Get photo by id
-// Public
+//- GET /api/photos/:id
+//- Get photo by id
+//- Public
 exports.getPhotoById = async (req, res) => {
   try {
     const { id } = req.params;
@@ -35,13 +34,13 @@ exports.getPhotoById = async (req, res) => {
 
     res.status(200).json({ data });
   } catch (error) {
-    res.status(500).json({ message: 'Server error. Please try again later.' });
+    res.status(404).json({ message: 'Not Found.' });
   }
 };
 
-// GET /api/photos/user/:username
-// Get photo by id
-// Public
+//- GET /api/photos/user/:username
+//- Get photo by id
+//- Public
 exports.getUserPhotos = async (req, res) => {
   try {
     const { username } = req.params;
